d474726abfcc800b0e6c50d0cff6acbbf7f6a49d
tlvlp <tllvllp@gmail.com>
2024-07-07 12:22:34 +0200

    bump

--- pu.sh
+++ pu.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+
 echo "Rebuilding Nixos"
 sudo nixos-rebuild switch --flake .#default\
 && echo "Pushing changes to origin"\
+&& if [ -z "$1" ]; then echo "Commit message was not set, using default!"; fi\
 && git add .\
 && git commit -m "${1:-bump}"
-# && git push origin main
+&& git push origin main
