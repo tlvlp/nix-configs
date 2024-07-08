263973ba3f7898f96f985a4740b0d928cce8b447
tlvlp <tllvllp@gmail.com>
2024-07-08 02:35:21 +0200

    bump

--- home.nix
+++ home.nix
@@ -28,8 +28,10 @@
     # Gruvbox Theme
     gtk = {
         enable = true;
-        theme.name = "Gruvbox-Dark-BL";
+        theme.name = "Gruvbox-Dark-BL-GS";
         theme.package = pkgs.gruvbox-gtk-theme;
+        gtk3.extraConfig = { Settings = "gtk-application-prefer-dark-theme = 1";};
+        gtk4.extraConfig = { Settings = "gtk-application-prefer-dark-theme = 1";};
     };
     programs.vscode = {
       extensions = with pkgs.vscode-extensions; [
