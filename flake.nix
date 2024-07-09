40223907ae54908b34373692d6c1012a6f19f177
tlvlp <tllvllp@gmail.com>
2024-07-09 23:39:14 +0200

    Attempt theming.

--- flake.nix
+++ flake.nix
@@ -7,6 +7,8 @@
             url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";
         };
+        nix-colors.url = "github:misterio77/nix-colors";
+
     };
 
     outputs = { self, nixpkgs, ... } @ inputs : {
