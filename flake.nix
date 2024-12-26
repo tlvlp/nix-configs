4a338331aee10c585c810f7e01a59048d4b9463c
tlvlp <tllvllp@gmail.com>
2024-12-27 00:22:46 +0100

    upgrading nixos to stable 24.11

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
